5daa9a3d6f5e2a3bdfba7b8fed52ec43293d9d9a
rzkyaffan <affandirrh@gmail.com>
2023-12-24 14:42:30 +0700

    menambahkan seluruh file

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}' , 'index.html'],
+  theme: {
+
+    container: {
+      center: true,
+      padding : '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        aboutBg : '#f7f7f7',
+        textBg : '#f68da6',
+        btnBg : '#e74d83'
+      },
+
+      screens:{
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
